a7ce8975a1d6701a08b67a24437c8541ae3541d0
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-03-13 14:41:26 -0300

    Feat(nvim): Add highlight on yank settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -101,4 +101,13 @@ vim.api.nvim_set_keymap("n", "<leader>sv", ":luafile ~/.config/nvim/init.lua<cr>
 
 -- }}}
 
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight",
+  { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    require("vim.highlight").on_yank({ timeout = 5000 })
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
 -- vim: foldmethod=marker: foldlevel=0
