f1de161a60adb240215dfd9b68a402a1d18c5651
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-03-15 19:19:03 -0300

    Feat(nvim): Add some treesitter settings

--- .config/nvim/lua/setup/tree-sitter.lua
+++ .config/nvim/lua/setup/tree-sitter.lua
@@ -1,6 +1,9 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
+  -- A list of parser names, or "all"
+  ensure_installed = "all",
+
   highlight = {
-    enable = true
+    enable = true,
+    additional_vim_regex_highlighting = false,
   },
-  additional_vim_regex_highlighting = false,
 }
