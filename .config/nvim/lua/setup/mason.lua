8b29dbd93f59d139947277c30ac5014ec83c2f42
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2023-03-20 16:36:32 -0300

    Refactor(nvim)!: Remove formatting user command

--- .config/nvim/lua/setup/mason.lua
+++ .config/nvim/lua/setup/mason.lua
@@ -47,10 +47,6 @@ local on_attach = function(_, bufnr)
       })
     end,
   })
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, "Format",
-  function(_) vim.lsp.buf.format() end,
-  { desc = "Format current buffer with LSP" })
 end
 
 -- The following language servers will automatically be installed. Use their
