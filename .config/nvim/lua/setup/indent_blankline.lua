59e3c9641e5faad4d2dfad88340a289a3c768e76
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2024-02-14 13:06:50 -0300

    Feat: Update indent_blankline settings

--- .config/nvim/lua/setup/indent_blankline.lua
+++ .config/nvim/lua/setup/indent_blankline.lua
@@ -1,8 +1,1 @@
-require("indent_blankline").setup({
-  char = "|",
-  max_indent_increase = 1,
-  context_char = "┊",
-  show_current_context = true,
-  show_current_context_start = true,
-  show_trailing_blankline_indent = false,
-})
+require("ibl").setup({})
