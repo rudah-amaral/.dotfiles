97a28c4ea3c660a6bfe5c99423528c920850656c
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2025-02-02 22:54:33 -0300

    Feat(Mason): Add clangd LSP for C programming

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -34,7 +34,7 @@ return {
       end
 
       nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-      -- nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+      nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
       nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
       nmap("gr", require("telescope.builtin").lsp_references,
@@ -69,6 +69,7 @@ return {
       texlab = {},
       ts_ls = {},
       vimls = {},
+      clangd = {}
     }
 
     mason_lspconfig.setup({
