df0b7446b154e6fd78589706fe6707ef7d2bcfef
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2025-12-31 16:37:39 -0300

    Feat: Add lsp configs

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,18 @@
+local nmap = require("mapper").nmap
+
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
   config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
     -- vim.lsp.enable("markdown")
-    vim.lsp.enable("cssls")
     vim.lsp.enable("arduino_language_server")
+    vim.lsp.enable("cssls")
     vim.lsp.enable("eslint")
     vim.lsp.enable("html")
     vim.lsp.enable("javascript")
@@ -12,5 +21,9 @@ return {
     vim.lsp.enable("nixd")
     vim.lsp.enable("tombi")
     vim.lsp.enable("ts_ls")
+    vim.lsp.enable("ty")
+
+    nmap("gd", ":lua vim.lsp.buf.definition()<CR>")
+    nmap("<leader>rn", ":lua vim.lsp.buf.rename()<CR>")
   end
 }
